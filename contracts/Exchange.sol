b85e8b2d59760b948bc04d034d625e0640eb3232
Joon2000 <najoonwon22@gmail.com>
2024-04-13 12:28:10 +0900

    Fix to complete

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,22 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 
 contract Exchange is ERC20 {
     address public tokenAddress;
 
-    event TokenPurchase(address indexed buyer, uint256 indexed etherSold, uint256 indexed tokensBought);
-    event EtherPurchase(address indexed buyer, uint256 indexed tokensSold, uint256 indexed etherBought);
-    event AddLiquidity(address indexed provider, uint256 indexed etherAmount, uint256 indexed tokenAmount);
-    event RemoveLiquidity(address indexed provider, uint256 indexed etherAmount, uint256 indexed tokenAmount);
+    event TokenPurchase(
+        address indexed buyer,
+        uint256 indexed etherSold,
+        uint256 indexed tokensBought
+    );
+    event EtherPurchase(
+        address indexed buyer,
+        uint256 indexed tokensSold,
+        uint256 indexed etherBought
+    );
+    event AddLiquidity(
+        address indexed provider,
+        uint256 indexed etherAmount,
+        uint256 indexed tokenAmount
+    );
+    event RemoveLiquidity(
+        address indexed provider,
+        uint256 indexed etherAmount,
+        uint256 indexed tokenAmount
+    );
 
-
-    constructor(
-        address _tokenAddress
-    ) ERC20("Uniswap V1", "UNI-V1") {
+    constructor(address _tokenAddress) ERC20("Uniswap V1", "UNI-V1") {
         tokenAddress = _tokenAddress;
     }
 
@@ -36,17 +47,17 @@ contract Exchange is ERC20 {
 
             _mint(msg.sender, lpTokenAmount);
             emit AddLiquidity(msg.sender, etherAmount, tokenAmount);
-            
+
             return lpTokenAmount;
         } else {
             // Liquidity Exists: (etherReserve, tokenReserve)
             uint etherReserve = address(this).balance - msg.value;
             uint tokenReserve = token.balanceOf(address(this));
-            
+
             // (Share) = msg.value / etherReserve
-            lpTokenAmount = totalLiquidity * msg.value / etherReserve;
-            uint tokenAmount = tokenReserve * msg.value / etherReserve;
-            
+            lpTokenAmount = (totalLiquidity * msg.value) / etherReserve;
+            uint tokenAmount = (tokenReserve * msg.value) / etherReserve;
+
             require(lpTokenAmount >= minLpTokenAmount);
             require(maxTokenAmount >= tokenAmount);
 
@@ -65,13 +76,15 @@ contract Exchange is ERC20 {
         // Implemented in removing liquidity with fair share of Ether and Tokens.
         uint totalLiquidity = totalSupply();
         ERC20 token = ERC20(tokenAddress);
-        etherAmount = address(this).balance * lpTokenAmount / totalLiquidity;
-        tokenAmount = token.balanceOf(address(this)) * lpTokenAmount / totalLiquidity;
+        etherAmount = (address(this).balance * lpTokenAmount) / totalLiquidity;
+        tokenAmount =
+            (token.balanceOf(address(this)) * lpTokenAmount) /
+            totalLiquidity;
 
         _burn(msg.sender, lpTokenAmount);
         payable(msg.sender).transfer(etherAmount);
         token.transfer(msg.sender, tokenAmount);
-        
+
         emit RemoveLiquidity(msg.sender, etherAmount, tokenAmount);
 
         return (etherAmount, tokenAmount);
@@ -84,11 +97,14 @@ contract Exchange is ERC20 {
     /// @param outputReserve The total reserve of output token
     /// @return outputAmount The amount of output token that will be received
     function getInputPrice(
-        uint inputAmount,
-        uint inputReserve,
-        uint outputReserve
+        uint inputAmount, //A
+        uint inputReserve, //X
+        uint outputReserve //Y
     ) public pure returns (uint outputAmount) {
-        outputAmount = inputAmount * 967 / 1000;  // CSMM
+        uint numerator = inputAmount * outputReserve;
+        uint denominator = inputAmount + inputReserve;
+        uint outputAmountWithFee = numerator / denominator;
+        outputAmount = (outputAmountWithFee * 967) / 1000; // CSMM
     }
 
     /// @notice Calculate the input amount required for a trade given the output amount, input reserve, and output reserve
@@ -98,11 +114,15 @@ contract Exchange is ERC20 {
     /// @param outputReserve The total reserve of output token
     /// @return inputAmount The amount of input token that needs to be provided
     function getOutputPrice(
-        uint outputAmount,
-        uint inputReserve,
-        uint outputReserve
+        uint outputAmount, //B
+        uint inputReserve, //X
+        uint outputReserve //Y
     ) public pure returns (uint inputAmount) {
-        inputAmount = outputAmount * 1000 / 967;  // CSMM
+        //XB/(Y-B) = A
+        uint numerator = inputReserve * outputAmount;
+        uint denominator = outputReserve - outputAmount;
+        uint inputAmountWithFee = numerator / denominator;
+        inputAmount = (inputAmountWithFee * 1000) / 967; // CSMM
     }
 
     function etherToTokenInput(
@@ -112,14 +132,14 @@ contract Exchange is ERC20 {
         uint etherSold = msg.value;
         ERC20 token = ERC20(tokenAddress);
         tokensBought = getInputPrice(
-            etherSold, 
+            etherSold,
             address(this).balance - msg.value,
             token.balanceOf(address(this))
         );
 
         // Validity
         require(tokensBought >= minTokens);
-        
+
         // Transfer
         token.transfer(msg.sender, tokensBought);
 
